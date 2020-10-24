c3d4ae5876ee6729ccf5bd702bf054c72c73ad7d
stephaneworkspace <s.bressani@bluewin.ch>
2020-10-25 01:47:39 +0200

    ok

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,45 +1,77 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>Stéphane Bressani homepage</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      Welcome to my homepage... and to all others
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Ukulele helper</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
+      <li>
+        <a
+          href="https://ukulele.stephane-bressani.ch"
+          target="_blank"
+          rel="noopener"
+          >Ukulele Helper</a
+        >
+      </li>
     </ul>
-    <h3>Essential Links</h3>
+    <h3>My music project</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li>
+        <a href="https://www.electryps.ch" target="_blank" rel="noopener"
+          >electryps.ch</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://www.soundcloud.com/electryps"
+          target="_blank"
+          rel="noopener"
+          >soundcloud.com/electryps</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://www.soundcloud.com/solar-empire"
+          target="_blank"
+          rel="noopener"
+          >soundcloud.com/solar-empire</a
+        >
+      </li>
     </ul>
-    <h3>Ecosystem</h3>
+    <h3>Astrology theme</h3>
     <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li>
+        <a
+          href="https://www.astrologie-traditionnelle.net"
+          target="_blank"
+          rel="noopener"
+          >astrologie-traditionnelle.net</a
+        >
+      </li>
+    </ul>
+    <h3>Code</h3>
+    <ul>
+      <li>
+        <a
+          href="https://www.github.com/stephaneworkspace"
+          target="_blank"
+          rel="noopener"
+          >My github page</a
+        >
+      </li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String,
-  },
+    msg: String
+  }
 });
 </script>
 
